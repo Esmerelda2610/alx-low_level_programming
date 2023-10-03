4782cf0151a4d5c73d38394c3c98c318f6ccffc7
Elizabeth2610 <mahlatsemotsinone@gmail.com>
2023-10-03 10:30:30 -0700

    my libmy.a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *@dest: input value 8
+ *@src: input value
+ *Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int j;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	jile (src[j] != '\0')
+	
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
