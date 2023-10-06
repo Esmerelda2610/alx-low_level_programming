622343b2d132adbb16535d86bdd871e9f5256d6e
Elizabeth2610 <mahlatsemotsinone@gmail.com>
2023-10-06 14:13:06 -0700

    Trust no one

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -8,6 +9,5 @@ int *array_range(int min, int max);
 int is_digit(char *s);
 int _strlen(char *s);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
 
 #endif
