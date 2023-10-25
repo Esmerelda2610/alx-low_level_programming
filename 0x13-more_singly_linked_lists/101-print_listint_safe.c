227616f0a0bb94613d790d95a6b2878e9a750b75
Elizabeth2610 <mahlatsemotsinone@gmail.com>
2023-10-25 12:40:13 -0700

    Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,32 +7,34 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tmp_n = NULL;
-	const listint_t *l_n = NULL;
-	size_t counter = 0;
-	size_t new_n;
+	size_t count = 0;
+	size_t new_node;
+	const listint_t *current = NULL;
+	const listint_t *prev = NULL;
 
-	tmp_n = head;
-	while (tmp_n)
+	current = head;
+
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
-		counter++;
-		tmp_n = tmp_n->next;
-		l_n = head;
-		new_n = 0;
-		while (new_n < counter)
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		current = current->next;
+		prev = head;
+		new_node = 0;
+
+		while (new_node < count)
 		{
-			if (tmp_n == l_n)
+			if (current == prev)
 			{
-				printf("->[%p] %d\n", (void *)tmp_n, tmp_n->n);
-				return (counter);
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (count);
 			}
-			l_n = l_n->next;
-			new_n++;
+			prev = prev->next;
+			new_node++;
 		}
-		if (!head)
+		if (head == NULL)
 			exit(98);
 	}
-	return (counter);
-
+	return (count);
 }
+
