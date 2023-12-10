4e046a638915e2a2ea1e2a7b23d0344abe406bbe
Elizabeth2610 <mahlatsemotsinone@gmail.com>
2023-12-10 07:19:36 -0800

    Doubly linked lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,39 +1,46 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds a new node at the end
- * of a dlistint_t list
- *
- * @head: head of the list
- * @n: value of the element
- * Return: the address of the new element
- */
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list
+ * @head: a double pointer to the head of the list
+ * @n: an integer data for the new node
+ * Return: address of the new element, NULL if fail
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *h;
-	dlistint_t *new;
+	dlistint_t *new_node;
+	dlistint_t *current;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new-> = n;
-	new->next = NULL;
-
-	h = *head;
-
-	if (h != NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 	{
-		while (h->next != NULL)
-			h = h->next;
-		h->next = new;
+		return (NULL);
+	}
+	/*Create a new node, set data to n, next pointer to NULL*/
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{ /*If list is empty, set prev pointer to NULL*/
+		new_node->prev = NULL;
+		/*Update head of list to new node*/
+		*head = new_node;
 	}
 	else
-	{
-		*head = new;
+	{ /*If not empty, traverse list to find last node*/
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		/*
+		* Set to link next pointer of last node
+		* and prev pointer of new node
+		*/
+		current->next = new_node;
+		new_node->prev = current;
 	}
 
-	new->prev = h;
-
-	return (new);
+	return (new_node);
 }
+
