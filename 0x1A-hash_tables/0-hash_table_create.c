581c2fe7e4b8a4ed3836b8ee339df85b3e3d16b5
Elizabeth2610 <mahlatsemotsinone@gmail.com>
2023-12-21 14:43:51 -0800

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ * Return: a pointer to newly created hash table, NULL if fail
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int j;
+
+	/*Allocate memory for the new hash_table_t struct*/
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+	/* initialize the size number*/
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (j = 0; j < size; j++)
+		ht->array[j] = NULL;
+
+	return (ht);
+}
